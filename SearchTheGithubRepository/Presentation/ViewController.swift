a1af2e79720ba2ab9b03c2d222814aa6f92282f5
beomcheol.shin <beomcheol.shin@wesang.com>
2024-09-21 15:04:15 +0900

    #2 add search bar

--- SearchTheGithubRepository/Presentation/ViewController.swift
+++ SearchTheGithubRepository/Presentation/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  private lazy var searchController: UISearchController = {
+    let searchController = UISearchController(searchResultsController: nil)
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "저장소 검색"
+    searchController.searchBar.showsCancelButton = true
+    return searchController
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    navigationItem.searchController = searchController
+    navigationItem.hidesSearchBarWhenScrolling = false
   }
 
+}
 
+extension ViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    
+  }
+  
 }
 
